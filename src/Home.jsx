8bddd2b85a559fd102c58534869575a6a7aa11e3
Adiljutt264 <adiljutt.264@gmail.com>
2024-01-02 22:22:15 +0500

    useContext and useRef Hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,16 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useContext, useRef} from 'react';
+import { Context } from './index';
+
+const Row = () => {
+    const data = useContext(Context);
+    const customElement = useRef(null);
+    useEffect(()=>{
+        console.log(customElement.current);
+    })
+  return (
+    <div ref={customElement}>{data}</div>
+  )
+}
 
 const Home = () => {
     const [a, setA] = useState(0);
@@ -10,6 +22,7 @@ const Home = () => {
         <button onClick={()=> setA(a + 1)}>+</button>
         <p>{a}</p>
         <button onClick={()=> setA(a-1)}>-</button>
+        <Row/>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { createContext } from "react";
+const Context  = createContext();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Context.Provider value={"Adil"}>
     <App />
+    </Context.Provider>
   </React.StrictMode>
 );
 
+export {Context};
